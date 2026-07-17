16024482d9675ed21224564cc41e4feae8c08aff
Heshan Nadeera <heshanx@iMac-Pro.local>
2026-07-17 08:41:13 -0700

    Created game layout

--- Tapfrenzy/ContentView.swift
+++ Tapfrenzy/ContentView.swift
@@ -9,11 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            Text("Score: 0")
+                .font(.title2)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            
+            Text("Time: 10")
+                .font(.title3)
+            
+            Button(action: {
+                
+            }) {
+                Text("Tap")
+                    .font(.largeTitle)
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .frame(width: 200, height: 200)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+            }
         }
         .padding()
     }
