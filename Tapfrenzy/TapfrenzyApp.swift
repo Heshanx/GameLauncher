4438c3ae0c5c4be87812d193cf946ccee017704d
Heshan Nadeera <heshanx@iMac-Pro.local>
2026-07-17 08:11:57 -0700

    Initial SwiftUI Project Setup

---
+++ Tapfrenzy/TapfrenzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapfrenzyApp.swift
+//  Tapfrenzy
+//
+//  Created by Heshan Nadeera on 2026-07-17.
+//
+
+import SwiftUI
+
+@main
+struct TapfrenzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
